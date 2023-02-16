3ce6eaafb2382dbab100ac38f8565de5ad262c2b
Nicola Strappazzon C <nstrappazzonc@gmail.com>
2023-02-16 17:19:24 +0100

    rename table_definition to definition.

--- plugins/definition/main.go
+++ plugins/definition/main.go
@@ -1,4 +1,4 @@
-package table_definition
+package definition
 
 import (
 	"fmt"
@@ -8,15 +8,15 @@ import (
 	"mysql-ddl-lint/plugins/registry"
 )
 
-type TableDefinition struct {
+type Definition struct {
 	Property registry.Property
 }
 
 func init() {
-	registry.Add("Table Definition", func() registry.Method { return &TableDefinition{} })
+	registry.Add("Table Definition", func() registry.Method { return &Definition{} })
 }
 
-func (m *TableDefinition) Run(p registry.Property) {
+func (m *Definition) Run(p registry.Property) {
 	m.Property = p
 	m.Property.Code = 300
 
@@ -37,11 +37,11 @@ func (m *TableDefinition) Run(p registry.Property) {
 	}
 }
 
-func (m *TableDefinition) AddMessage(id int, message string) {
+func (m *Definition) AddMessage(id int, message string) {
 	m.Property.Messages = append(m.Property.Messages, registry.Message{Code: id, Message: message})
 }
 
-func (m *TableDefinition) IsTable() {
+func (m *Definition) IsTable() {
 	ex := `CREATE\sTABLE\s\x60.*\x60\s\([\s\S]*\).*`
 	match, err := regexp.MatchString(ex, m.Property.Table.Raw)
 	if match == false || err != nil {
@@ -49,61 +49,61 @@ func (m *TableDefinition) IsTable() {
 	}
 }
 
-func (m *TableDefinition) Engine() {
+func (m *Definition) Engine() {
 	if m.Property.Table.Engine != "InnoDB" {
 		m.AddMessage(2, "Table engine is not InnoDB.")
 	}
 }
 
-func (m *TableDefinition) Charset() {
+func (m *Definition) Charset() {
 	if !strings.Contains(strings.ToLower(m.Property.Table.Charset), "utf8") {
 		m.AddMessage(3, "Table charset is not set to use UTF8.")
 	}
 }
 
-func (m *TableDefinition) Collate() {
+func (m *Definition) Collate() {
 	if !strings.Contains(strings.ToLower(m.Property.Table.Collate), "utf8") {
 		m.AddMessage(4, "Table collate is not set to use UTF8.")
 	}
 }
 
-func (m *TableDefinition) Comment() {
+func (m *Definition) Comment() {
 	if len(m.Property.Table.Comment) == 0 {
 		m.AddMessage(5, "Table no have description.")
 	}
 }
 
-func (m *TableDefinition) Name() {
+func (m *Definition) Name() {
 	if len(m.Property.Table.Name) == 0 {
 		m.AddMessage(6, "Table no have name.")
 	}
 }
 
-func (m *TableDefinition) Length() {
+func (m *Definition) Length() {
 	if len(m.Property.Table.Name) > 64 {
 		m.AddMessage(7, fmt.Sprintf("Table name is large: %s.", m.Property.Table.Name))
 	}
 }
 
-func (m *TableDefinition) Dots() {
+func (m *Definition) Dots() {
 	if strings.Contains(m.Property.Table.Name, ".") {
 		m.AddMessage(8, fmt.Sprintf("Table name contains dot's in the name: %s.", m.Property.Table.Name))
 	}
 }
 
-func (m *TableDefinition) StartWithUnderscore() {
+func (m *Definition) StartWithUnderscore() {
 	if strings.HasPrefix(m.Property.Table.Name, "_") {
 		m.AddMessage(9, fmt.Sprintf("Table name start with underscore: %s.", m.Property.Table.Name))
 	}
 }
 
-func (m *TableDefinition) EndWithTemp() {
+func (m *Definition) EndWithTemp() {
 	if strings.HasSuffix(m.Property.Table.Name, "_tmp") || strings.HasSuffix(m.Property.Table.Name, "_temp") {
 		m.AddMessage(10, fmt.Sprintf("Table name end with _tmp or _temp: %s.", m.Property.Table.Name))
 	}
 }
 
-func (m *TableDefinition) LowerCase() {
+func (m *Definition) LowerCase() {
 	for _, r := range m.Property.Table.Name {
 		if r >= 'A' && r <= 'Z' {
 			m.AddMessage(11, fmt.Sprintf("Table name has capital letter: %s.", m.Property.Table.Name))
--- plugins/main.go
+++ plugins/main.go
@@ -1,8 +1,8 @@
 package plugins
 
 import (
+	_ "mysql-ddl-lint/plugins/definition"
 	_ "mysql-ddl-lint/plugins/field"
 	_ "mysql-ddl-lint/plugins/file"
 	_ "mysql-ddl-lint/plugins/primary_key"
-	_ "mysql-ddl-lint/plugins/table_definition"
 )
